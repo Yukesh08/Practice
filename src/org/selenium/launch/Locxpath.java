86d7699e8bac551d8a542820aa162d2229a4ae2a
yukesh08 <yukesh08@gmail.com>
2019-06-29 18:11:40 +0530

    Selenium project added

---
+++ src/org/selenium/launch/Locxpath.java
@@ -0,0 +1,20 @@
+package org.selenium.launch;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Locxpath {
+public static void main(String[] args) {
+	System.setProperty("webdriver.chrome.driver","E:\\Selenium\\chromedriver.exe");
+	WebDriver driver = new ChromeDriver();
+	driver.get("http://www.greenstechnologys.com/");
+	WebElement btn = driver.findElement(By.xpath("//a[@href='contact.php']"));
+	btn.click();
+	driver.findElement(By.id("InputName")).sendKeys("Yukaish");
+	driver.findElement(By.id("InputEmaill")).sendKeys("yukesh08@gmail.com");
+	driver.findElement(By.id("InputSubject")).sendKeys("9884953010");
+driver.findElement(By.name("courses")).sendKeys("Java");
+}
+}
