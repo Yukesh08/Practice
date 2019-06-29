a5d54870f7f8772d5355599727de34ff5203e832
yukesh08 <yukesh08@gmail.com>
2019-06-29 12:45:25 +0530

    Selenium project added

--- src/org/selenium/launch/LaunchBrowser.java
+++ src/org/selenium/launch/LaunchBrowser.java
@@ -1,17 +1,15 @@
 
 package org.selenium.launch;
 
-import org.openqa.selenium.By;
+
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+
 import org.openqa.selenium.chrome.ChromeDriver;
 public class LaunchBrowser {
 public static void main(String[] args) {
 	System.setProperty("webdriver.chrome.driver","E:\\Selenium\\chromedriver.exe");
 	WebDriver driver = new ChromeDriver();
-	driver.get("http://www.greenstechnologys.com/");
-	WebElement txt = driver.findElement(By.xpath("//hd[text()='Greens Technologys Overall Reviews']"));
-	String act = txt.getText();
-	System.out.println(act);
+	driver.get("https://www.flipkart.com/");
+	
 }
 }
